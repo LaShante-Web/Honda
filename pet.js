b27853eec1df079af45869a3c5ae0d6ef6854628
LaShante Thomas <Lrthomas3@yahoo.com>
2021-06-23 21:06:48 -0400

    adding pictures

---
+++ pet.js
@@ -0,0 +1,34 @@
+function greet () {
+    return "Hello world!"; 
+
+}
+
+
+function substract () {
+    return 10-5; 
+}
+
+
+console.log(subtract());
+
+
+let pup_info = {
+    name: "Thinkpup",
+    weight: 6,
+    happiness: 5
+
+}
+
+console.log(pet_info)
+
+function petWeight() {
+    let pet_weight = 6
+    if (pet_weights > 10){
+        console.log("Your pet is unhealty.")
+    } else {
+        console.log("Your Pet is Healhty")
+    }
+
+
+}
+
